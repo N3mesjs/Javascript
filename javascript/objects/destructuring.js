7fcd710e5741aec440e670d4281da7df75935ee5
N3mesjs <alessiodelsorbo390@gmail.com>
2026-02-09 17:30:32 +0100

    .

--- javascript/objects/destructuring.js
+++ javascript/objects/destructuring.js
@@ -42,3 +42,65 @@ let {
 
 console.log(tit);
 console.log(ui);
+
+
+let preflightHeader = {
+    ...options,
+};
+
+const array = [1,2,3,4,5,6];
+const newAr = [...array, 7,8];
+console.log(array);
+console.log(...array);
+console.log(array)
+
+/**
+ * NOTE: () => {} arrow functions, beyond this
+ * keyword, don't also have the arguments keyword
+ * so we are forced to use the ...rest method
+ */
+
+/**
+ * Returns the sum of all the numbers given
+ * 
+ * @param  {...any} numbers 
+ * @returns {number}
+ * This function uses the spread operator to get all the number
+ * you give as parameter, it must be the last argument of the
+ * function, and it give you an array of the parameters
+ */
+
+function sum(...numbers) {
+    let count = 0;
+
+    for (let val of numbers) {
+        count += val;
+    }
+
+    return count
+}
+
+console.log(sum(1,2,3,4,5,6,7,8,9,10));
+
+/**
+ * 
+ * @returns {number} the sum of all numbers
+ * 
+ * This function behave the same way as the one made before
+ * but it uses an old method called the arguments keyword
+ * but it comes with a some withdrawals, it is like an 
+ * array but its not an arraya, so you cannot access
+ * to array methods, like .map, etc...
+ */
+
+function sum() {
+    let count = 0;
+
+    for (let val of arguments) {
+        count += val;
+    }
+
+    return count
+}
+
+console.log(sum(1,2,3,4,5,6,7,8,9,10));
