15237cba21c0eced544b12302f7e9e1bfafcd2df
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2026-02-26 13:50:22 +0100

    prototypes review

---
+++ javascript/objects/prototype/test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let animal = {
+    walk() {
+        console.log('I am walking');
+    },
+    eat() {
+        console.log('I am eating');
+    }
+}
+
+function Rabbit(name){
+    this.name = name;
+}
+
+let rat = {
+    run() {
+        console.log('I am running!')
+    },
+}
+rat.__proto__ = animal;
+
+Rabbit.prototype = animal
+
+const bunny = new Rabbit('Bugs Bunny')
+
+bunny.eat();
+console.log(Object.getOwnPropertyDescriptors(bunny));
+console.dir(bunny);
+console.dir(rat);
